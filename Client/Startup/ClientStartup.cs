459c3dd4b61a8b74a57506b06da774baf0a64966
Alireza Tavakoli <developer1-ictace@outlook.com>
2025-11-13 22:52:45 +1100

    Add Syncfusion Blazor and user secrets support

--- Client/Startup/ClientStartup.cs
+++ Client/Startup/ClientStartup.cs
@@ -1,7 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using Oqtane.Services;
 using ICTAce.FileHub.Services;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 
 namespace ICTAce.FileHub.Startup
 {
@@ -13,6 +16,12 @@ namespace ICTAce.FileHub.Startup
             {
                 services.AddScoped<IMyModuleService, MyModuleService>();
             }
+
+            services.AddSyncfusionBlazor();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetService<IConfiguration>();
+            SyncfusionLicenseProvider.RegisterLicense(configuration["Syncfusion:LicenseKey"]);
         }
     }
 }
