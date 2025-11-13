6256efc6f82bf4460af963848a33cb8ad0eeca18
Behnam Emamian <ben@ictace.com>
2025-11-13 23:03:16 +1100

    add bunit

---
+++ Client.Tests/CounterCSharpTest.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Client.Tests;
+
+/// <summary>
+/// These tests are written entirely in C#.
+/// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+/// </summary>
+public class CounterCSharpTest : BunitContext
+{
+    [Fact]
+    public void CounterStartsAtZero()
+    {
+        // Arrange
+        var cut = Render<Counter>();
+
+        // Assert that content of the paragraph shows counter at zero
+        cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+    }
+
+    [Fact]
+    public void ClickingButtonIncrementsCounter()
+    {
+        // Arrange
+        var cut = Render<Counter>();
+
+        // Act - click button to increment counter
+        cut.Find("button").Click();
+
+        // Assert that the counter was incremented
+        cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    }
+}
