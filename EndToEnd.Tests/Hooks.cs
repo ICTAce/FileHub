e0263d1a345de1af18281a56f1976d6db3cf65db
Behnam Emamian <ben@ictace.com>
2025-11-13 22:37:13 +1100

    add end2end

---
+++ EndToEnd.Tests/Hooks.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics;
+
+namespace ICTAce.FileHub.EndToEnd.Tests;
+
+public class Hooks
+{
+    [Before(TestSession)]
+    public static void InstallPlaywright()
+    {
+        if (Debugger.IsAttached)
+        {
+            Environment.SetEnvironmentVariable("PWDEBUG", "1");
+        }
+
+        Microsoft.Playwright.Program.Main(["install"]);
+    }
+}
---
+++ EndToEnd.Tests/Tests.cs
@@ -0,0 +1,32 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TUnit.Playwright;
+
+namespace ICTAce.FileHub.EndToEnd.Tests;
+
+public class Tests : PageTest
+{
+    [Test]
+    public async Task Test()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+        // create a locator
+        var getStarted = Page.Locator("text=Get Started");
+
+        // Expect an attribute "to be strictly equal" to the value.
+        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+        // Click the get started link.
+        await getStarted.ClickAsync();
+
+        // Expects the URL to contain intro.
+        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+    }
+}
