44149a33ab474e8418e99260a188cf1ee81312a6
Behnam Emamian <ben@ictace.com>
2025-11-13 23:18:30 +1100

    rename client test

--- Client.Tests/CounterCSharpTest.cs
+++ Client.Tests/CounterCSharpTest.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Client.Tests;
+namespace ICTAce.FileHub.Client.Tests;
 
 /// <summary>
 /// These tests are written entirely in C#.
