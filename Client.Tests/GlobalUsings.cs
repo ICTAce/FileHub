c08c862858a1f3f944d593f0f9a37084cf0d1cc0
Behnam Emamian <ben@ictace.com>
2025-11-18 09:57:34 +1100

    clean up usings

---
+++ Client.Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Licensed to ICTAce under the MIT license.
+
+global using Bunit;
+global using Bunit.TestDoubles;
+global using Microsoft.Extensions.DependencyInjection;
+global using TUnit.Core;
+global using TUnit.Assertions;
