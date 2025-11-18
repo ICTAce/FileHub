a0d10a50048346558e948dfecea56b5b2d9fd399
Behnam Emamian <ben@ictace.com>
2025-11-18 17:28:45 +1100

    splite settings

---
+++ Client/Modules/MyModule/Settings.razor.cs
@@ -0,0 +1,53 @@
+// Licensed to ICTAce under the MIT license.
+
+namespace ICTAce.FileHub.MyModule;
+
+public partial class Settings : ModuleBase
+{
+    [Inject]
+    protected ISettingService SettingService { get; set; } = default!;
+
+    [Inject]
+    protected IStringLocalizer<Settings> Localizer { get; set; } = default!;
+
+    private const string ResourceType = "ICTAce.FileHub.MyModule.Settings, ICTAce.FileHub.Client.Oqtane";
+    
+    public override string Title => "MyModule Settings";
+
+    private string _value = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            var settings = await SettingService.GetModuleSettingsAsync(ModuleState.ModuleId);
+            _value = SettingService.GetSetting(settings, "SettingName", string.Empty);
+        }
+        catch (Exception ex)
+        {
+            await HandleErrorAsync(ex);
+        }
+    }
+
+    public async Task UpdateSettings()
+    {
+        try
+        {
+            var settings = await SettingService.GetModuleSettingsAsync(ModuleState.ModuleId);
+            SettingService.SetSetting(settings, "SettingName", _value);
+            await SettingService.UpdateModuleSettingsAsync(settings, ModuleState.ModuleId);
+            
+            AddModuleMessage("Settings updated successfully", MessageType.Success);
+        }
+        catch (Exception ex)
+        {
+            await HandleErrorAsync(ex);
+        }
+    }
+
+    private async Task HandleErrorAsync(Exception ex)
+    {
+        AddModuleMessage(ex.Message, MessageType.Error);
+        await Task.CompletedTask;
+    }
+}
