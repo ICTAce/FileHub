935013c5b175e36f9e26f9c98ed8809c9aca76ff
Behnam Emamian <ben@ictace.com>
2025-12-01 00:03:32 +1100

    add e2e test

--- EndToEnd.Tests/GlobalUsings.cs
+++ EndToEnd.Tests/GlobalUsings.cs
@@ -1,5 +1,9 @@
 // Licensed to ICTAce under the MIT license.
 
-global using System.Text.RegularExpressions;
-global using TUnit.Playwright;
 global using System.Diagnostics;
+global using System.Text.RegularExpressions;
+global using Microsoft.Playwright;
+global using TUnit.Assertions;
+global using TUnit.Assertions.Extensions;
+global using TUnit.Core;
+global using TUnit.Playwright;
