6cda24d0b516ba4c71f38f499b0a952fc9970533
Behnam Emamian <ben@ictace.com>
2025-12-18 19:16:50 +1100

    move to settings

--- Client/Modules/FileHub/Settings.razor.cs
+++ Client/Modules/FileHub/Settings.razor.cs
@@ -14,35 +14,8 @@ public partial class Settings : ModuleBase
 
     public override string Title => "FileHub Settings";
 
-    private string _value = string.Empty;
-
     protected override async Task OnInitializedAsync()
     {
-        try
-        {
-            var settings = await SettingService.GetModuleSettingsAsync(ModuleState.ModuleId);
-            _value = SettingService.GetSetting(settings, "SettingName", string.Empty);
-        }
-        catch (Exception ex)
-        {
-            await HandleErrorAsync(ex);
-        }
-    }
-
-    public async Task UpdateSettings()
-    {
-        try
-        {
-            var settings = await SettingService.GetModuleSettingsAsync(ModuleState.ModuleId);
-            SettingService.SetSetting(settings, "SettingName", _value);
-            await SettingService.UpdateModuleSettingsAsync(settings, ModuleState.ModuleId);
-
-            AddModuleMessage("Settings updated successfully", MessageType.Success);
-        }
-        catch (Exception ex)
-        {
-            await HandleErrorAsync(ex);
-        }
     }
 
     private async Task HandleErrorAsync(Exception ex)
